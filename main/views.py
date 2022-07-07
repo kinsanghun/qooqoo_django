79aaf28bf0bb75929a0ee994b621c25135cafa85
kinsanghun <52707244+kinsanghun@users.noreply.github.com>
2022-07-07 22:38:01 +0900

    add report

--- main/views.py
+++ main/views.py
@@ -12,21 +12,26 @@ def index(request):
     year = datetime.now().year
     month = datetime.now().month
 
-    context = {
-        'selected': selected,
-        'year': year,
-        'month': month3(year, month),
-    }
-    return render(request, "main/index.html", context)
-
-def report(request):
+    #misu
     conn = tradeReport()
     client_misu = conn.client_misu()
     fix_misu = conn.fix_misu()
     rant_misu = conn.rant_misu()
+
     context = {
+        'selected': selected,
+        'year': year,
+        'month': month3(year, month),
+
+        #misu
         'client_misus': client_misu,
         'fix_misus': fix_misu,
+        'rant_misus': rant_misu,
+    }
+    return render(request, "main/index.html", context)
+
+def report(request):
+    context = {
 
     }
     return render(request, "main/report.html", context)
