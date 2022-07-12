c8b6e89fa73ca4eac89361ee07ed0a0fbb64786b
kinsanghun <52707244+kinsanghun@users.noreply.github.com>
2022-07-13 01:37:21 +0900

    add report

--- employee/urls.py
+++ employee/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "employee"
 
 urlpatterns = [
-    #path('employee/', views.employee, name="employee")
+    path('employee', views.employee, name="employee"),
+    path('parttimer', views.parttimer, name="parttimer"),
+    path('oneday', views.oneday, name="oneday"),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+def employee(request):
+    if request.method == "POST":
+        return redirect("employee:employee")
+
+    context = {
+    }
+    return render(request, "employee/employee.html", context)
+
+def parttimer(request):
+    if request.method == "POST":
+        return redirect("employee:parttimer")
+
+    context = {
+    }
+    return render(request, "employee/parttimer.html", context)
+
+def oneday(request):
+    if request.method == "POST":
+        return redirect("employee:oneday")
+
+    context = {
+    }
+    return render(request, "employee/oneday.html", context)
